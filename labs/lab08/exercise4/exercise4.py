94126d97e413fb5f6deadcf0f052e28f0d11748b
Willim2208 <m-9385950@moe-dl.edu.my>
2026-09-14 09:33:24 +0800

    lab8

--- labs/lab08/exercise4/exercise4.py
+++ labs/lab08/exercise4/exercise4.py
@@ -1,7 +1,11 @@
 current_reading = int(input())
 previous_reading = int(input())
 
+consumption = current_reading - previous_reading
 
+if consumption <= 20:
+    water_cost = consumption * 0.57
+elif (consumption > 20) and (consumption <= 35):
 
 print(consumption)
 print(water_cost)
